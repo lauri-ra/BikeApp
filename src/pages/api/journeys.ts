31bc7c7139a7e5e5e03712f60ddb6df13f09b834
lauri-ra <lrautiala+1@gmail.com>
2023-01-28 23:55:39 +0200

    Journeys api route test

--- src/pages/api/journeys.ts
+++ src/pages/api/journeys.ts
@@ -1,6 +1,9 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../../prisma/connectPrisma';
 
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-	res.status(200).json('journeys');
+export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
+	const journeys = await prisma.journeys.findMany();
+
+	console.log(typeof journeys);
+	res.status(200).json(journeys);
 }
