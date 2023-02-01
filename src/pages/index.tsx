0dc43d6a726f3a736ac4d0ccde9d517e3ae8fe76
lauri-ra <lrautiala+1@gmail.com>
2023-02-02 00:11:16 +0200

    changes to main and journey list styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,19 +7,22 @@ export default function Home() {
 			<Head>
 				<title>BikeApp</title>
 			</Head>
-			<main className='h-screen bg-slate-900 text-white'>
-				<div className='flex justify-center text-3xl'>BikeApp</div>
-				<div className='flex justify-center pt-10'>
-					<Link href='/journeys'>
-						<button className='mr-3 rounded-md bg-sky-500 px-5 py-2.5 font-semibold text-white hover:bg-sky-400'>
-							Journeys
-						</button>
-					</Link>
-					<Link href='#'>
-						<button className='mr-3 rounded-md bg-sky-500 px-5 py-2.5 font-semibold text-white hover:bg-sky-400'>
-							Stations
-						</button>
-					</Link>
+			<main className='flex h-screen items-center justify-center'>
+				<div>
+					<div className='mb-10 flex justify-center text-6xl tracking-wide'>BikeApp</div>
+					<div className='flex justify-center text-lg'>
+						<Link href='/journeys'>
+							<button className='mr-4 rounded-lg bg-sky-500 px-6 py-4 font-semibold text-white hover:bg-sky-400'>
+								Journeys
+							</button>
+						</Link>
+						<Link href='#'>
+							<button className='ml-4 rounded-lg bg-sky-500 px-6 py-4 font-semibold text-white hover:bg-sky-400'>
+								Stations
+							</button>
+						</Link>
+					</div>
+					{/* <Image src={bikers} alt='Picture of some bikers' /> */}
 				</div>
 			</main>
 		</>
--- src/pages/journeys.tsx
+++ src/pages/journeys.tsx
@@ -21,23 +21,23 @@ export default function Journeys({ journeyList }: InferGetStaticPropsType<typeof
 	};
 
 	return (
-		<div className='h-screen bg-slate-900 text-white'>
-			<div className='flex justify-center pt-5 text-3xl'>Journeys</div>
+		<div className='h-screen bg-neutral-100'>
+			<div className='flex justify-center pt-5 text-4xl'>Journey List</div>
 
 			<div className='flex justify-center py-12'>
 				<form>
 					<input
-						placeholder='Search...'
+						placeholder='Search by station'
 						value={filter}
 						onChange={handleSearch}
-						className='block w-60 rounded-lg border border-gray-600 bg-gray-700 p-2 text-center text-white outline-none hover:bg-gray-600 focus:border-sky-400 focus:drop-shadow-md'
+						className='block w-60 rounded-lg border bg-neutral-200 p-2 text-left text-gray-600 outline-none drop-shadow-md hover:border-sky-400 focus:border-sky-400'
 					></input>
 				</form>
 			</div>
 
-			<div className='relative mx-8 overflow-x-auto rounded-lg ring-1 ring-slate-600'>
+			<div className='relative mx-8 overflow-x-auto rounded-lg ring-1 ring-neutral-300'>
 				<table className='w-full table-auto content-center text-left'>
-					<thead className='bg-slate-600 text-xs font-thin uppercase'>
+					<thead className='bg-neutral-300 text-xs font-thin uppercase text-gray-700 drop-shadow'>
 						<tr>
 							<th className='px-6 py-3'>Departure station</th>
 							<th className='px-6 py-3'>Return station</th>
@@ -47,11 +47,11 @@ export default function Journeys({ journeyList }: InferGetStaticPropsType<typeof
 					</thead>
 					<tbody>
 						{filteredJourneys.map((journey) => (
-							<tr key={journey.id} className='hover:bg-slate-800'>
-								<td className='px-6 py-2'>{journey.departure_station_name}</td>
-								<td className='px-6 py-2'>{journey.return_station_name}</td>
-								<td className='px-6 py-2'>{(journey.covered_distance_m / 1000).toFixed(2)}</td>
-								<td className='px-6 py-2'>{(journey.duration_s / 60).toFixed(0)}</td>
+							<tr key={journey.id} className='hover:bg-neutral-300'>
+								<td className='px-6 py-2.5'>{journey.departure_station_name}</td>
+								<td className='px-6 py-2.5'>{journey.return_station_name}</td>
+								<td className='px-6 py-2.5'>{(journey.covered_distance_m / 1000).toFixed(2)}</td>
+								<td className='px-6 py-2.5'>{(journey.duration_s / 60).toFixed(0)}</td>
 							</tr>
 						))}
 					</tbody>
